4e5700460b58bd8d9e17bc50bb0890bc09767358
ameya-6964 <belvalkar7@gmail.com>
2023-01-03 00:43:02 +0530

    Done With About Section

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='App' >
-      <h2> Adress </h2>
+    <div className='about' >
+      <section className='gym-details'>
+       <h1> High Class Gym Equipments </h1>
+       <button>Explore Our Equipments</button>
+      </section>
+      <section className='male'>
+      <h1> Dedicated Male Section </h1>
+      <button> Click For More Information</button>
+      </section>
+      <section className='female'>
+      <h1> Best Female Trainers Available </h1>
+      <button>Click For More Information</button>
+      </section>
     </div>
   )
 }
--- src/Components/Packages.js
+++ src/Components/Packages.js
@@ -1,8 +1,20 @@
 import React from 'react';
 const Packages = () => {
   return (
-    <div className='App'>
-      <h1> Hey I Am Gym Packages </h1>
+    <div>
+
+    <div className='Packages-1-month'>
+      <h1> We Have 1 Month Package Worth for <del>1500 /- ₹</del> Offer Price : 1000 ₹ </h1>
+    </div>
+    <div className='Packages-3-month'>
+      <h1> We Have 3 Month Package Worth for <del> 3500 /- ₹</del> Offer Price : 2500 ₹ </h1>
+    </div>
+    <div className='Packages-6-month'>
+      <h1> We Have 6 Month Package Worth for <del>6500 /- ₹</del> Offer Price : 5000 ₹ </h1>
+    </div>
+    <div className='Packages-12-month'>
+      <h1> We Have Annual Package Worth for <del>8500 /- ₹</del> Offer Price : 6000  ₹ </h1>
+    </div>
     </div>
   )
 }
