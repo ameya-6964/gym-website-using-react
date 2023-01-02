0b921f8daf33f4b3b8ef21d72f06edb0b31bc111
ameya-6964 <belvalkar7@gmail.com>
2023-01-03 01:10:43 +0530

    Completed

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
-const FSD = () => {
+const Contact = () => {
   return (
-    <div className='App'>
-      <h2> I Am Contact </h2>
+    <div className='contact'>
+            <h1>Contact Us</h1>
+          <form >
+            <div class="Personal-details">
+              <label for="name">Name</label>
+              <input id="name" type="text" required/>
+              <label for="email">Email</label>
+              <input id="email" type="email" required />
+              <label for="mobile">Number</label>
+              <input id="Mobile" type="number" required />
+            </div>             
+            <button>Request Call Back</button>
+          </form>
     </div>
   )
 }
 
-export default FSD
+export default Contact
--- src/Components/Packages.js
+++ src/Components/Packages.js
@@ -3,17 +3,15 @@ const Packages = () => {
   return (
     <div>
 
-    <div className='Packages-1-month'>
+    <div className='Packages-Monthly'>
       <h1> We Have 1 Month Package Worth for <del>1500 /- ₹</del> Offer Price : 1000 ₹ </h1>
-    </div>
-    <div className='Packages-3-month'>
       <h1> We Have 3 Month Package Worth for <del> 3500 /- ₹</del> Offer Price : 2500 ₹ </h1>
-    </div>
-    <div className='Packages-6-month'>
+      <button> Buy Now </button>
+     </div>
+    <div className='Packages-Annual'>
       <h1> We Have 6 Month Package Worth for <del>6500 /- ₹</del> Offer Price : 5000 ₹ </h1>
-    </div>
-    <div className='Packages-12-month'>
       <h1> We Have Annual Package Worth for <del>8500 /- ₹</del> Offer Price : 6000  ₹ </h1>
+      <button> Buy Now </button>
     </div>
     </div>
   )
